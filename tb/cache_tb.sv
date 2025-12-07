133e043a9bb1b4e5364e3ac86e0dd787ddefc724
Danton Klein <dantonmklein@gmail.com>
2025-12-06 19:13:01 -0500

    fucked up in the crib sippin dr perky

--- tb/cache_tb.sv
+++ tb/cache_tb.sv
@@ -1,3 +1,53 @@
+module simulated_ram #(
+    parameter int ADDRESS_WIDTH = 16
+) (
+    input logic clk, rst,
+
+    input logic[31:0] ram_data_wr,
+    input logic[ADDRESS_WIDTH-1:0] ram_address,
+    input logic ram_rd, ram_wr,
+    input logic [3:0] ram_byte_enable,
+    output logic [31:0] ram_data_rd,
+    output logic ram_ready
+);
+    localparam int ram_size = 2 ** (ADDRESS_WIDTH-2);
+
+    logic[7:0] ram[ram_size][4];
+
+    initial begin
+        for(int i = 0; i < ram_size; i++) begin
+            ram[i][0] = i[7:0];
+            ram[i][1] = i[15:8];
+            ram[i][2] = i[23:16];
+            ram[i][3] = i[31:24];
+        end
+    end
+
+    always_ff @(posedge clk or posedge rst) begin
+        if(rst) begin
+            ram_data_rd <= 0;
+            ram_ready <= 0;
+        end else begin
+            ram_ready <= 0;
+
+            if(ram_rd) begin
+                ram_ready <= 1;
+                ram_data_rd[31:24] <= ram[ram_address[ADDRESS_WIDTH-1:2]][3];
+                ram_data_rd[23:16] <= ram[ram_address[ADDRESS_WIDTH-1:2]][2];
+                ram_data_rd[15:8] <= ram[ram_address[ADDRESS_WIDTH-1:2]][1];
+                ram_data_rd[7:0] <= ram[ram_address[ADDRESS_WIDTH-1:2]][0];
+            end else if(ram_wr) begin
+                ram_ready <= 1;
+                if(ram_byte_enable[0]) ram[ram_address[ADDRESS_WIDTH-1:2]][0] <= ram_data_wr[7:0];
+                if(ram_byte_enable[1]) ram[ram_address[ADDRESS_WIDTH-1:2]][1] <= ram_data_wr[15:8];
+                if(ram_byte_enable[2]) ram[ram_address[ADDRESS_WIDTH-1:2]][2] <= ram_data_wr[23:16];
+                if(ram_byte_enable[3]) ram[ram_address[ADDRESS_WIDTH-1:2]][3] <= ram_data_wr[31:24];
+            end
+        end
+    end
+
+endmodule
+
 module cache_tb;
 
     localparam int ADDRESS_WIDTH = 16;
